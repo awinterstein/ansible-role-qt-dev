af6ee8753f3ed53c130e3bbbbaac235543d1e39b
Adrian Winterstein <dev@cerritus.eu>
2018-03-24 12:51:37 +0100

    Added scripts for cross compiling Qt for the Raspberry Pi.

---
+++ templates/sysroot-sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RASPI_ADDRESS=${RASPI_ADDRESS:="{{raspi_address}}"}
+RASPI_USER=${RASPI_USER:="{{raspi_user}}"}
+SYSROOT_DIR="{{toolsdir}}/raspberrypi-qt/sysroot/"
+
+mkdir -p "$SYSROOT_DIR"
+
+rsync -avz --delete $RASPI_USER@$RASPI_ADDRESS:/lib :/usr/include :/usr/lib :/opt/vc "$SYSROOT_DIR"
+
+sysroot-relativelinks "$SYSROOT_DIR"
+
+tar cfj "{{homedir}}/raspi_sysroot.tbz2" "$SYSROOT_DIR"
