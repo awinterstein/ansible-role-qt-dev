c7cd1009710888c1180575308e2417cbe4a12a43
Adrian Winterstein <dev@cerritus.eu>
2019-12-01 15:34:17 +0100

    Updates Qt to 5.12.4.

--- templates/cross-compile-qt.sh
+++ templates/cross-compile-qt.sh
@@ -4,7 +4,7 @@ RASPI_VERSION=linux-rasp-pi2-g++
 SYSROOT_DIR="{{toolsdir}}/raspberrypi-qt/sysroot/"
 
 cd "{{toolsdir}}/qt-{{qt_version_major}}/{{qt_version_full}}/Src/"
-./configure -release -opengl es2 -device $RASPI_VERSION -device-option CROSS_COMPILE="{{toolsdir}}/raspberrypi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-" -sysroot "$SYSROOT_DIR" -opensource -confirm-license -make libs -make tools -prefix /usr/local/qt -extprefix "{{toolsdir}}/raspberrypi-qt/qt5pi/" -hostprefix "{{toolsdir}}/raspberrypi-qt/qt5/" -no-use-gold-linker -skip qtwebengine -v
+./configure -release -opengl es2 -device $RASPI_VERSION -device-option CROSS_COMPILE="{{toolsdir}}/raspberrypi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-" -sysroot "$SYSROOT_DIR" -opensource -confirm-license -make libs -make tools -prefix /usr/local/qt -extprefix "{{toolsdir}}/raspberrypi-qt/qt5pi/" -hostprefix "{{toolsdir}}/raspberrypi-qt/qt5/" -no-use-gold-linker -no-gbm -skip qtwebengine -v
 
 make -j8
 make install
